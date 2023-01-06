bf68619e7183518e6fab52b4c83a56f982e9b64c
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-06 21:05:21 +0000

    new

---
+++ newec2/ec2.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+    region      = "us-east-1"
+}
+
+resource "aws_instance" "demo2" {
+  ami                       = "ami-00ff427d936335825"
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
+
+  tags     = {
+    Name   = "EC2-Terraform-Instance2"
+  }
+}
+
+output "public_ip" {
+    value  = aws_instance.demo.public_ip
+}
+
+# Creates security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from INTERNET"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssg_sg"
+  }
+}
