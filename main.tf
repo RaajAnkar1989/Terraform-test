285f804e2a1815c87e86e9d4563955e19e93fac1
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-06 20:51:41 +0000

    new

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo1" {
   ami                       = "ami-00ff427d936335825"
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = [aws_security_group1.allow_ssh.id]
+  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
 
   tags     = {
     Name   = "EC2-Terraform-Instance1"
@@ -13,11 +13,11 @@ resource "aws_instance" "demo1" {
 }
 
 output "public_ip" {
-    value  = aws_instance.demo.public_ip
+    value  = aws_instance.demo1.public_ip
 }
 
 # Creates security group
-resource "aws_security_group1" "allow_ssh" {
+resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh1"
   description = "Allow SSH inbound traffic"
 
