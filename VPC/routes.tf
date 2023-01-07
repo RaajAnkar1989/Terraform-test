3e825d37482d0e82f0206f76a6abb8a3b785593c
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-07 23:46:21 +0000

    new

--- VPC/routes.tf
+++ VPC/routes.tf
@@ -1,4 +1,12 @@
-resource "aws_route" "r" {
-  route_table_id            = "rtb-4fbb3ac4"
-  destination_cidr_block    = "10.0.0.0/17"
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+  
 }
