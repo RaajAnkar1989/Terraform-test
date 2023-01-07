437411dca80df1f90bb95735c12610023999f27a
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-07 19:53:00 +0000

    new

--- Ec2/main.tf
+++ Ec2/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo1"  {
   ami                       = "ami-00ff427d936335825"
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = [var.sg]
+  vpc_security_group_ids    = ["var.sg"]
 
   tags     = {
     Name   = "EC2-Terraform-Instance"
