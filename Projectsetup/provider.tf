5d0ae0e672e2ab6ef568105a2dca0a731b892fec
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-07 20:32:14 +0000

    new

--- Projectsetup/provider.tf
+++ Projectsetup/provider.tf
@@ -1,20 +1,11 @@
- # terraform {
- #   backend "s3" {
- #     bucket = "raajankar"
- #     key    = "project/env/terrafom.tfstate"
- #     region = "us-east-1"
- #    }
- # }
-
-provider "aws" {
-    region = "us-east-1"
-}
-
-
-   # terraform {
+# terraform {
    #   backend "s3" {
    #     bucket = "raajankar"
    #     key    = "project/env/terrafom.tfstate"
    #     region = "us-east-1"
    #    }
    # }
+
+provider "aws" {
+    region = "us-east-1"
+}
