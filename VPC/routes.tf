f1f880d2f2461066593a747857f8f353f44d4a4f
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-07 23:58:06 +0000

    new

--- VPC/routes.tf
+++ VPC/routes.tf
@@ -10,3 +10,25 @@ resource "aws_route_table" "rt" {
   }
   
 }
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.a.id
+}
+
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.0.0/17"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "private-rt"
+  }
+  
+  resource "aws_route_table_association" "a1" {
+  subnet_id      = aws_subnet.main1.id
+  route_table_id = aws_route_table.a1.id
+}
+}
