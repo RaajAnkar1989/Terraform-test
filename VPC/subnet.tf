9a78b73a22c8a98e27376ade5a558b16120a66fa
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-07 23:18:39 +0000

    new

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -6,3 +6,12 @@ resource "aws_subnet" "main" {
     Name = "Public-subnet"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.128.0/17"
+
+  tags = {
+    Name = "Private-subnet"
+  }
+}
