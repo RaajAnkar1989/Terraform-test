e5938d426af419a3634beace625436561632e547
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-07 18:44:25 +0000

    new

--- Ec2/main.tf
+++ Ec2/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region      = "us-east-1"
   }
 
-resource "aws_instance" "New-server"  {
+resource "aws_instance" "Newserver"  {
   ami                       = "ami-00ff427d936335825"
   instance_type             = "t2.micro"
   vpc_security_group_ids    = [var.sg]
